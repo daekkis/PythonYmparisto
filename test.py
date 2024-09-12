c489ceea55608c7042bdfd7eff4883cbe35ece35
daekkis <156065221+daekkis@users.noreply.github.com>
2024-09-12 09:44:20 +0300

    Otsikoita

---
+++ test.py
@@ -0,0 +1,12 @@
+# Tämä on ensimmäinen testiohjelma python-kielellä
+# ================================================
+
+# Kirjastot
+# ---------
+
+# Asetukset
+# ---------
+
+# Pääohjelma
+# ----------
+
